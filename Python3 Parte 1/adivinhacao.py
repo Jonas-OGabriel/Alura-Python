3fa7609db1ba095269dab25e095a7c4be5191480
Jonas-OGabriel <joao.gabriel.araujo.salvador@gmail.com>
2022-01-02 17:11:39 -0300

    Codificação completa do adivinhacao.py

--- Python3 Parte 1/adivinhacao.py
+++ Python3 Parte 1/adivinhacao.py
@@ -1,1 +1,23 @@
-print("Bem vindo ao jogo de adivinhação!")
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+
+#O código abaixo salvará o input do usuário como string
+#chute = input("Digite o seu numero: ")
+
+#O código int() converte o valor de input para tipo int
+chute = int(input("Digite o seu numero: "))
+
+print("Você digitou", chute)
+
+if(numero_secreto == chute):
+    print("Você acertou!")
+else:
+    print("Você errou")
+
+print("Fim do jogo!")
+
+#O código acima sempre dará "Você errou" pois os tipos das variaveis são diferentes
+#print(type(numero_secreto), type(chute))
