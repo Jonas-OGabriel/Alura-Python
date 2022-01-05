e1ca6c1cdace21bbc67eac8ed4502e5421eaba75
Jonas-OGabriel <joao.gabriel.araujo.salvador@gmail.com>
2022-01-05 13:11:37 -0300

    Adicionado loop while para tentivas

--- Python3 Parte 1/adivinhacao.py
+++ Python3 Parte 1/adivinhacao.py
@@ -3,19 +3,26 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
+tentiva_atual = 1
 
-tentativa = int(input("Digite um número e tente acertar o número secreto: "))
+#Adicionando loop para tentativas com laço "while"
 
-condicao_acerto = tentativa == numero_secreto
-condicao_erro_maior = tentativa > numero_secreto
-condicao_erro_menor = tentativa < numero_secreto
+while(tentiva_atual <= 3):
+    print("Tentaiva", tentiva_atual ,"de 3")
+    tentativa = int(input("Digite um número e tente acertar o número secreto: "))
 
-if(condicao_acerto):
-    print("Parabens! Você acertou!")
-else:
-    if(condicao_erro_maior):
-        print("Que pena, você errou... Sua tentiva foi maior do que número secreto")
-    elif(condicao_erro_menor):
-        print("Que pena, você errou... Sua tentiva foi menor do que número secreto")
+    condicao_acerto = tentativa == numero_secreto
+    condicao_erro_maior = tentativa > numero_secreto
+    condicao_erro_menor = tentativa < numero_secreto
+
+    if(condicao_acerto):
+        print("Parabens! Você acertou!")
+    else:
+        if(condicao_erro_maior):
+            print("Que pena, você errou... Sua tentiva foi maior do que número secreto")
+        elif(condicao_erro_menor):
+            print("Que pena, você errou... Sua tentiva foi menor do que número secreto")
+    
+    tentiva_atual = tentiva_atual + 1
 
 print("Fim do jogo!")
