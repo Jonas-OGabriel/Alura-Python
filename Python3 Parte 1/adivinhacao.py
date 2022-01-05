a4ce1a24564161c056521538bd8d306791bff292
Jonas-OGabriel <joao.gabriel.araujo.salvador@gmail.com>
2022-01-05 13:52:11 -0300

    Formatando string da primeira função print() para melhor legibilidade

--- Python3 Parte 1/adivinhacao.py
+++ Python3 Parte 1/adivinhacao.py
@@ -5,10 +5,9 @@ print("*********************************")
 numero_secreto = 42
 tentiva_atual = 1
 
-#Adicionando loop para tentativas com laço "while"
-
 while(tentiva_atual <= 3):
-    print("Tentaiva", tentiva_atual ,"de 3")
+    #Formatando a string da função print()
+    print("Tentaiva {} de 3".format(tentiva_atual))
     tentativa = int(input("Digite um número e tente acertar o número secreto: "))
 
     condicao_acerto = tentativa == numero_secreto
