1939b58a4daf7eb60a57d8a1364cd40eb3e9ff16
Jonas-OGabriel <joao.gabriel.araujo.salvador@gmail.com>
2022-01-05 14:07:18 -0300

    Mudança de laço de repetição de while para for

--- Python3 Parte 1/adivinhacao.py
+++ Python3 Parte 1/adivinhacao.py
@@ -3,10 +3,10 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
-tentiva_atual = 1
 
-while(tentiva_atual <= 3):
-    #Formatando a string da função print()
+#Mudando loop while para adicionar o loop for
+for tentiva_atual in range(1,4):
+
     print("Tentaiva {} de 3".format(tentiva_atual))
     tentativa = int(input("Digite um número e tente acertar o número secreto: "))
 
@@ -21,7 +21,6 @@ while(tentiva_atual <= 3):
             print("Que pena, você errou... Sua tentiva foi maior do que número secreto")
         elif(condicao_erro_menor):
             print("Que pena, você errou... Sua tentiva foi menor do que número secreto")
-    
-    tentiva_atual = tentiva_atual + 1
+
 
 print("Fim do jogo!")
