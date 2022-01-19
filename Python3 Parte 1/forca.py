40995923340d673de950fc2803ee7068821532ea
Jonas-OGabriel <joao.gabriel.araujo.salvador@gmail.com>
2022-01-18 22:02:56 -0300

    Adicionado condições de vitória e derrota

--- Python3 Parte 1/forca.py
+++ Python3 Parte 1/forca.py
@@ -3,26 +3,41 @@ def jogar():
     print("****Bem vindo ao Jogo da Forca****")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"] 
+
     enforcou = False
     acertou = False
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"] 
+
+    erros = 0
+    
     
     print(letras_acertadas)
     
     while(not enforcou and not acertou):
         
         chute = input("Qual letra?: ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        indice = 0
-        for letra in palavra_secreta:
-            if(chute.lower() == letra.lower()):
-                letras_acertadas[indice] = letra
-            indice = indice + 1
-            
+        if chute in palavra_secreta:
+            indice = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[indice] = letra
+                indice += 1
+        else:
+            erros += 1
+            print("Ops, você errou! Faltam {} tentativas.".format(6-erros))
+        
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         
         print("{}\njogando...".format(letras_acertadas))
+    
+    if(acertou):
+        print("Você ganhou!")
+    else:
+        print("Você Perdeu!")
 
 
     print("Fim do Jogo")
