407fdcb25354b87f7ccdeb3a2b395bbaf36833c8
Jonas-OGabriel <joao.gabriel.araujo.salvador@gmail.com>
2022-01-18 21:12:12 -0300

    Adicionado lista para guardar acertos do usuário

--- Python3 Parte 1/forca.py
+++ Python3 Parte 1/forca.py
@@ -6,19 +6,23 @@ def jogar():
     palavra_secreta = "banana"
     enforcou = False
     acertou = False
-
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"] 
+    
+    print(letras_acertadas)
+    
     while(not enforcou and not acertou):
         
         chute = input("Qual letra?: ")
         chute = chute.strip()
 
+        indice = 0
         for letra in palavra_secreta:
-            if(chute.lower() == letra):
-                print(chute, end="")
-            else:
-                print("_", end="")
-
-        print("\njogando...")
+            if(chute.lower() == letra.lower()):
+                letras_acertadas[indice] = letra
+            indice = indice + 1
+            
+        
+        print("{}\njogando...".format(letras_acertadas))
 
 
     print("Fim do Jogo")
