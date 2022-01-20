a6b7da957f0d1014e2db25f8054e256bd7911407
Jonas-OGabriel <joao.gabriel.araujo.salvador@gmail.com>
2022-01-20 20:26:29 -0300

    Adicionado classe Playlist

--- Python OO 2/modelo.py
+++ Python OO 2/modelo.py
@@ -1,3 +1,6 @@
+from validators import length
+
+
 class TV_program:
 
     def __init__(self, program_name, program_year):
@@ -37,21 +40,53 @@ class Series(TV_program):
     def __str__(self):
         return "Name: {} --- Year: {} --- Seasons: {} --- Likes: {}".format(self.program_name, self.program_year, self.series_duration, self.likes_quantity)
 
+class Playlist:
+    
+    def __init__(self, playlist_name, program_list):
+        self._playlist_name = playlist_name
+        self._program_list = program_list
+    
+    @property
+    def program_list(self):
+        return self._program_list
+    
+    @property
+    def playlist_name(self):
+        return self._playlist_name
+     
+
 def test():
 
     movie1 = Movie("test Movie name", 1997, 165)
+    movie2 = Movie("test Movie name 2", 2007, 250)
+    movie3 = Movie("test Movie name 3", 2017, 35)
     series1 = Series("test series Name", 2000, 5)
+    series2 = Series("test series Name 2", 2010, 2)
+    series3 = Series("test series Name 3", 2020, 3)
 
     movie1.give_like()
     movie1.give_like()
+    movie2.give_like()
+    movie3.give_like()
+    movie3.give_like()
+    movie3.give_like()
 
     series1.give_like()
     series1.give_like()
-    series1.give_like()
+    series2.give_like()
+    series2.give_like()
+    series2.give_like()
+    series2.give_like()
+    series2.give_like()
+    series3.give_like()
 
-    list_programs = [movie1, series1]
+    list_of_all_programs = [movie1, movie2, movie3, series1, series2, series3]
 
-    for programs in list_programs:
+    weekend_playlist = Playlist("test", list_of_all_programs)
+
+    print("Tamanho da lista é: {}".format(len(weekend_playlist.program_list)))
+
+    for programs in weekend_playlist.program_list:
         print(programs)
 
 if( __name__ == "__main__"):
