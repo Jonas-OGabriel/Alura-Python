2c986a8b07ec8c98a23a2ecfb83fe2556a5c5cd7
Jonas-OGabriel <joao.gabriel.araujo.salvador@gmail.com>
2022-01-20 17:07:21 -0300

    Definido propriedades, setters e staticmethod; adicionado método privado

--- Python OO/conta.py
+++ Python OO/conta.py
@@ -2,12 +2,13 @@
 
 class Conta:
 
-    def __init__(self, numero_conta, titular_conta, saldo_atual, limite_credito):
+    def __init__(self, numero_conta = 0, titular_conta = "Teste", saldo_atual = 100.0, limite_credito = 1000.0):
         print("Contruindo objeto... {}".format(self))
         self.__numero = numero_conta
         self.__titular = titular_conta
         self.__saldo = saldo_atual
         self.__limite = limite_credito
+
 
     def extrato(self):
         print("Saldo: {} do titular {}".format(self.__saldo, self.__titular))
@@ -18,26 +19,43 @@ class Conta:
         print("Extrato:")
         self.extrato()
 
+    def __pode_sacar(self, valor_saque):
+        valor_disponivel_para_saque = self.__saldo + self.__limite
+        return valor_saque <= valor_disponivel_para_saque
+
     def sacar(self, valor_saque):
         print("Sacando {} do saldo".format(valor_saque))
-        self.__saldo -= valor_saque
-        print("Extrato:")
-        Conta.extrato(self)
+        if(self.__pode_sacar(valor_saque)):
+            self.__saldo -= valor_saque
+            print("Extrato:")
+            Conta.extrato(self)
+        else:
+            print("Limite insuficiente para saque")
 
     def transferir(self, valor_tranferencia, conta_destino):
         self.sacar(valor_tranferencia)
         conta_destino.depositar(valor_tranferencia)
     
-    def get_saldo(self):
-        return self.get_saldo
+    @property
+    def saldo(self):
+        return self.__saldo
     
     @property
     def limite(self):
         return self.__limite
 
-    def get_titular(self):
-        return self.get_titular
+    @property
+    def titular(self):
+        return self.__titular
 
     @limite.setter
     def limite(self, novo_limite):
         self.__limite = novo_limite
+    
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco':'237'}
