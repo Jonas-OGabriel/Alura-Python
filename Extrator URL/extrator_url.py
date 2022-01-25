706e9d395876cd8dc7ac1e0ee9c99124243ffc03
Jonas-OGabriel <joao.gabriel.araujo.salvador@gmail.com>
2022-01-24 21:04:32 -0300

    Finalização do curso + desafio extra

--- Extrator URL/extrator_url.py
+++ Extrator URL/extrator_url.py
@@ -1,10 +1,13 @@
 import re
+from decimal import Decimal
 
 class ExtratorURL:
 
     def __init__(self, url):
         self.url = self.sanitize_url(url)
         self.validate_url()
+        self._dolar_value = Decimal('5.50')
+        self.set_output()
 
     def sanitize_url(self, dirt_url):
         if type(dirt_url) == str:
@@ -47,17 +50,53 @@ class ExtratorURL:
 
         return parameter_value
 
+    def __len__(self):
+        return len(self.url)
+
+    def __str__(self):
+        return self.url + "\n" + "Parâmetros da URL: " + self.get_parameter_url() + "\n" + "URL Base: " + self.get_base_url()
+
+    def __eq__(self, other):
+        return self.url == other.url
+    
+    def get_conversion_value(self):
+        if self.get_parameter_value('moedaOrigem') == "dolar":
+            final_value = Decimal(self.get_parameter_value('quantidade')) * self._dolar_value
+        else:
+            final_value = Decimal(self.get_parameter_value('quantidade')) / self._dolar_value
+        return Decimal(final_value)
+    
+    def set_output(self):
+        print("A moeda origem é: $ {:0.2f} {}(s)".format(float(self.get_parameter_value('quantidade')), self.get_parameter_value('moedaOrigem')))
+        print("Sendo covertido para: $ {:0.2f} {}(s)".format(self.get_conversion_value(), self.get_parameter_value('moedaDestino')))
+        
+        
 
 def test(test_url = None):
     test = ExtratorURL(test_url)
+    """ 
+    #Esta parte é apenas para testar pontos específicos do código no decorrer das aulas
+
     print("A moeda origem é:")
     print(test.get_parameter_value('moedaOrigem'))
     print("A moeda destino é:")
     print(test.get_parameter_value('moedaDestino'))
+    
+    #Para comparar se está certo
+    print(len(test)==len("https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"))
+
+    #Para testar __str__
+    print(test)
+
+    #Para testar __eq__
+    test2 = ExtratorURL(test_url)
+    print(test == test2)
+    print(test is test2) # 'is' utiliza-se para saber se a IDENTIDADE de ambos valore é igual e.g: print( 1 is True) 
+     """
 
 
 
 
 if __name__ == '__main__':
-    #Para testar uma url: "https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
-    test('https://bytebank/cambio?moedaDestino=dolar&moedaOrigem=real')
+    #Para testar uma url: "https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real&quantidade=100"
+    test("https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real&quantidade=100")
