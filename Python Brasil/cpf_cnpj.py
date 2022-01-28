b779ab2eb62ec9507d403701050631cd56ff93fe
Jonas-OGabriel <joao.gabriel.araujo.salvador@gmail.com>
2022-01-28 13:48:31 -0300

    Refatoração para aplicar Design Pattern Factory

--- Python Brasil/cpf_cnpj.py
+++ Python Brasil/cpf_cnpj.py
@@ -1,5 +1,58 @@
 from validate_docbr import CPF, CNPJ
 
+class Document():
+
+    @staticmethod
+    def get_document_type(document_number):
+        if len(document_number) == 11:
+            return DocCPF(document_number)
+        elif len(document_number) == 14:
+            return DocCNPJ(document_number)
+        else:
+            raise ValueError('Quantidades de digitos invalido')
+
+class DocCPF():
+
+    def __init__(self, document_number):
+        if self.validate_cpf(document_number):
+            self.cpf = document_number
+        else:
+            raise ValueError('CPF invalido')
+
+    def __str__(self):
+        return self.format_presentation()
+
+
+    def validate_cpf(self, document_number):
+       validator = CPF()
+       return validator.validate(document_number)
+    
+    def format_presentation(self):
+        mask = CPF()
+        return mask.mask(self.cpf)
+
+
+
+class DocCNPJ():
+    
+    def __init__(self, document_number):
+        if self.validate_cnpj(document_number):
+            self.cnpj = document_number
+        else:
+            raise ValueError('CPF invalido')
+
+    def __str__(self):
+        return self.format_presentation()
+
+
+    def validate_cnpj(self, document_number):
+       validator = CNPJ()
+       return validator.validate(document_number)
+    
+    def format_presentation(self):
+        mask = CNPJ()
+        return mask.mask(self.cnpj)
+
 class CpfCnpj:
     
     def __init__(self, doc_number, doc_type):
@@ -20,7 +73,10 @@ class CpfCnpj:
 
 
     def __str__(self):
-        return self.format_visualization_cpf()
+        if self.doc_type == 'cpf':
+            return self.format_visualization_cpf()
+        elif self.doc_type == 'cnpj':
+            return self.format_visualization_cnpj()
     
     def validate_cpf(self, doc_number_str):
         if len(doc_number_str) == 11:
@@ -39,3 +95,7 @@ class CpfCnpj:
             return validator.validate(doc_number_str)
         else:
             raise ValueError('Digitos insuficientes')
+    
+    def format_visualization_cnpj(self):
+        cnpj_mask = CNPJ()
+        return cnpj_mask.mask(self.cnpj)
--- Python Brasil/main.py
+++ Python Brasil/main.py
@@ -1,9 +1,9 @@
-from cpf_cnpj import CpfCnpj
+from cpf_cnpj import Document
 
-#cpf = 22936850800
-#objeto_cpf = Cpf(cpf)
-#print(objeto_cpf)
+cpf = '22936850800'
+objeto_cpf = Document.get_document_type(cpf)
+print(objeto_cpf)
 
 cnpj = '35379838000112'
-objeto_cnpj = CpfCnpj(cnpj, 'cnpj')
-#print(objeto_cnpj)
+objeto_cnpj = Document.get_document_type(cnpj)
+print(objeto_cnpj)
